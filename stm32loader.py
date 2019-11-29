0dee9fba99f6f3f9e4e60defb6d4a29347f10922
Eagle Ding <dingfp@gmail.com>
2019-11-29 16:48:10 +0800

    changed into python3 and correct some CRC calculation

--- stm32loader.py
+++ stm32loader.py
@@ -5,6 +5,8 @@
 
 # Author: Ivan A-R <ivan@tuxotronic.org>
 # Project page: http://tuxotronic.org/wiki/projects/stm32loader
+# Modified by Eagle Ding <dingfp@hotmail.com>
+#   mainly change code to python3.x and correct some CRC checksum
 #
 # This file is part of stm32loader.
 #
@@ -45,13 +47,26 @@ chip_ids = {
     0x430: "STM32 XL-density",
     0x416: "STM32 Medium-density ultralow power line",
     0x411: "STM32F2xx",
-    0x413: "STM32F4xx",
+    0x450: "STM32H750",
 }
 
 def mdebug(level, message):
     if(QUIET >= level):
-        print >> sys.stderr , message
+        print(message) # >> sys.stderr , message
 
+FLASH_BASE_ADDR   = 0x90000000
+FLASH_SECTOR_SIZE = 0x10000       #64KBytes
+def prepare_sectors(address,data):
+    assert(address >= FLASH_BASE_ADDR)
+    data_len = len(data)
+    address  = address - FLASH_BASE_ADDR
+    sectors  = []
+    sector_addrs = range(address,data_len,FLASH_SECTOR_SIZE)
+    for sector_addr in sector_addrs:
+        sector_num = math.floor(sector_addr/FLASH_SECTOR_SIZE)
+        sectors.append( (sector_num<<8) & 0xFF )
+        sectors.append( sector_num & 0xFF )
+    return sectors
 
 class CmdException(Exception):
     pass
@@ -59,50 +74,73 @@ class CmdException(Exception):
 class CommandInterface:
     extended_erase = 0
 
-    def open(self, aport='/dev/tty.usbserial-ftCYPMYJ', abaudrate=115200) :
+    def open(self, aport='COM7', abaudrate=115200, aparity=serial.PARITY_NONE) :
+        global FLASH_BASE_ADDR
+        if(aparity == serial.PARITY_EVEN):
+            FLASH_BASE_ADDR = 0x08000000 #internal flash 
+        self.port = aport
+        self.baud = abaudrate
+        self.parity = aparity
         self.sp = serial.Serial(
             port=aport,
             baudrate=abaudrate,     # baudrate
             bytesize=8,             # number of databits
-            parity=serial.PARITY_EVEN,
+            parity=aparity,         #serial.PARITY_EVEN,
             stopbits=1,
             xonxoff=0,              # don't enable software flow control
             rtscts=0,               # don't enable RTS/CTS flow control
-            timeout=5               # set a timeout value, None for waiting forever
+            timeout=2               # set a timeout value, None for waiting forever
         )
-
+        
+    def reopen(self):
+        self.sp.close()
+        self.sp = serial.Serial(
+            port=self.port,
+            baudrate=self.baud,     # baudrate
+            bytesize=8,             # number of databits
+            parity=self.parity,#serial.PARITY_EVEN,
+            stopbits=1,
+            xonxoff=0,              # don't enable software flow control
+            rtscts=0,               # don't enable RTS/CTS flow control
+            timeout=2               # set a timeout value, None for waiting forever
+        )     
+        self.reset()
 
     def _wait_for_ask(self, info = ""):
         # wait for ask
         try:
-            ask = ord(self.sp.read())
+            #ask = ord(self.sp.read())
+            ask = self.sp.read()
+            #for a in ask:
+            #    print(hex(a))
         except:
             raise CmdException("Can't read port or timeout")
         else:
-            if ask == 0x79:
+            if ask[0] == 0x79:
                 # ACK
                 return 1
             else:
-                if ask == 0x1F:
+                if ask[0] == 0x1F:
                     # NACK
                     raise CmdException("NACK "+info)
                 else:
                     # Unknown responce
-                    raise CmdException("Unknown response. "+info+": "+hex(ask))
+                    raise CmdException("Unknown response. "+info+": "+ask.hex())
 
 
     def reset(self):
+        self.sp.flush()
         self.sp.setDTR(0)
-        time.sleep(0.1)
+        #time.sleep(0.01)
         self.sp.setDTR(1)
-        time.sleep(0.5)
+        #time.sleep(0.01)
 
     def initChip(self):
         # Set boot
         self.sp.setRTS(0)
-        self.reset()
-
-        self.sp.write("\x7F")       # Syncro
+        if(self.parity!=serial.PARITY_EVEN):
+            self.sp.write(b"\x55\x55\x55\x55\x55") # autobaud for qspi boot loader
+        self.sp.write(b"\x7F")       # Syncro
         return self._wait_for_ask("Syncro")
 
     def releaseChip(self):
@@ -110,8 +148,11 @@ class CommandInterface:
         self.reset()
 
     def cmdGeneric(self, cmd):
-        self.sp.write(chr(cmd))
-        self.sp.write(chr(cmd ^ 0xFF)) # Control byte
+        #time.sleep(0.001)
+        self.sp.write(bytes([cmd]))
+        #time.sleep(0.001)
+        self.sp.write(bytes([cmd ^ 0xFF])) # Control byte
+        #print(hex(cmd),hex(cmd ^ 0xFF))
         return self._wait_for_ask(hex(cmd))
 
     def cmdGet(self):
@@ -120,10 +161,12 @@ class CommandInterface:
             len = ord(self.sp.read())
             version = ord(self.sp.read())
             mdebug(10, "    Bootloader version: "+hex(version))
-            dat = map(lambda c: hex(ord(c)), self.sp.read(len))
-            if '0x44' in dat:
+            rx = self.sp.read(len)
+            #dat = map(lambda c: hex(ord(c)), self.sp.read(len))
+            dat = rx.hex()
+            if '44' in dat:
                 self.extended_erase = 1
-            mdebug(10, "    Available commands: "+", ".join(dat))
+            mdebug(10, "    Available commands: "+dat)#", ".join(dat))
             self._wait_for_ask("0x00 end")
             return version
         else:
@@ -146,7 +189,8 @@ class CommandInterface:
             len = ord(self.sp.read())
             id = self.sp.read(len+1)
             self._wait_for_ask("0x02 end")
-            return reduce(lambda x, y: x*0x100+y, map(ord, id))
+            return id[0]<<8 | id[1]
+            #return reduce(lambda x, y: x*0x100+y, map(ord, id))
         else:
             raise CmdException("GetID (0x02) failed")
 
@@ -157,20 +201,38 @@ class CommandInterface:
         byte1 = (addr >> 16) & 0xFF
         byte0 = (addr >> 24) & 0xFF
         crc = byte0 ^ byte1 ^ byte2 ^ byte3
-        return (chr(byte0) + chr(byte1) + chr(byte2) + chr(byte3) + chr(crc))
+        #return (chr(byte0) + chr(byte1) + chr(byte2) + chr(byte3) + chr(crc))
+        return bytes([byte0,byte1,byte2,byte3,crc])
 
 
     def cmdReadMemory(self, addr, lng):
         assert(lng <= 256)
         if self.cmdGeneric(0x11):
             mdebug(10, "*** ReadMemory command")
-            self.sp.write(self._encode_addr(addr))
+            #self.sp.write(self._encode_addr(addr))
+            addr_bytes = self._encode_addr(addr)
+            #print(len(addr_bytes),addr_bytes.hex())
+            for addr_byte in addr_bytes:
+                self.sp.write(bytes([addr_byte]))
+                #print(hex(addr_byte))
+                #time.sleep(0.001)
             self._wait_for_ask("0x11 address failed")
-            N = (lng - 1) & 0xFF
-            crc = N ^ 0xFF
-            self.sp.write(chr(N) + chr(crc))
+            
+            #self.reopen()
+            N = bytes( [(lng - 1) & 0xFF] )
+            crc = bytes( [(lng - 1) & 0xFF ^ 0xFF] )
+            #self.sp.write(chr(N) + chr(crc))
+            #self.cmdGeneric(N)
+            self.sp.write(N) #,N.hex())
+            #time.sleep(0.001)
+            self.sp.write(crc) #,crc.hex())
+            #time.sleep(0.001)
             self._wait_for_ask("0x11 length failed")
-            return map(lambda c: ord(c), self.sp.read(lng))
+            
+            rx = self.sp.read(lng)
+            #print(len(rx),rx.hex())
+            return rx.hex()
+            #return map(lambda c: ord(c), self.sp.read(lng))
         else:
             raise CmdException("ReadMemory (0x11) failed")
 
@@ -188,17 +250,28 @@ class CommandInterface:
         assert(len(data) <= 256)
         if self.cmdGeneric(0x31):
             mdebug(10, "*** Write memory command")
-            self.sp.write(self._encode_addr(addr))
+            #self.sp.write(self._encode_addr(addr))
+            addr_bytes = self._encode_addr(addr)
+            for addr_byte in addr_bytes:
+                self.sp.write(bytes([addr_byte]))
+                #print(hex(addr_byte))
+                #time.sleep(0.001)
             self._wait_for_ask("0x31 address failed")
             #map(lambda c: hex(ord(c)), data)
             lng = (len(data)-1) & 0xFF
-            mdebug(10, "    %s bytes to write" % [lng+1]);
-            self.sp.write(chr(lng)) # len really
-            crc = 0xFF
+            mdebug(10, "    %s bytes to write" % [lng+1])
+            self.sp.write(bytes([lng])) # len really
+            #time.sleep(0.001)
+            crc = lng #^ 0xFF
             for c in data:
                 crc = crc ^ c
-                self.sp.write(chr(c))
-            self.sp.write(chr(crc))
+                self.sp.write(bytes([c]))
+                #time.sleep(0.001)
+            self.sp.write(bytes([crc]))
+            #time.sleep(0.001)
+            #print(hex(crc))
+            #rx = self.sp.read(lng+4)
+            #print(rx.hex())
             self._wait_for_ask("0x31 programming failed")
             mdebug(10, "    Write memory done")
         else:
@@ -207,38 +280,52 @@ class CommandInterface:
 
     def cmdEraseMemory(self, sectors = None):
         if self.extended_erase:
-            return cmd.cmdExtendedEraseMemory()
+            return cmd.cmdExtendedEraseMemory(sectors)
 
         if self.cmdGeneric(0x43):
             mdebug(10, "*** Erase memory command")
             if sectors is None:
                 # Global erase
-                self.sp.write(chr(0xFF))
-                self.sp.write(chr(0x00))
+                self.sp.write(bytes([0xFF]))
+                self.sp.write(bytes([0x00]))
             else:
                 # Sectors erase
-                self.sp.write(chr((len(sectors)-1) & 0xFF))
+                self.sp.write(bytes([(len(sectors)-1) & 0xFF]))
                 crc = 0xFF
                 for c in sectors:
                     crc = crc ^ c
-                    self.sp.write(chr(c))
-                self.sp.write(chr(crc))
+                    self.sp.write(bytes([c]))
+                self.sp.write(bytes([c]))
             self._wait_for_ask("0x43 erasing failed")
             mdebug(10, "    Erase memory done")
         else:
             raise CmdException("Erase memory (0x43) failed")
 
-    def cmdExtendedEraseMemory(self):
+    def cmdExtendedEraseMemory(self,sectors = None):
         if self.cmdGeneric(0x44):
             mdebug(10, "*** Extended Erase memory command")
-            # Global mass erase
-            self.sp.write(chr(0xFF))
-            self.sp.write(chr(0xFF))
-            # Checksum
-            self.sp.write(chr(0x00))
+            if sectors is None:
+                # Global mass erase
+                self.sp.write(bytes([0xFF]))
+                self.sp.write(bytes([0xFF]))
+                # Checksum
+                self.sp.write(bytes([0x00]))
+            else:
+                # Sectors erase
+                sector_len = int(len(sectors)/2)-1
+                step1 = (sector_len>>8) & 0xFF
+                step2 = sector_len & 0xFF
+                self.sp.write(bytes([step1]))
+                self.sp.write(bytes([step2]))
+
+                crc = step1 ^ step2
+                for c in sectors:
+                    crc = crc ^ c
+                    self.sp.write(bytes([c]))
+                self.sp.write(bytes([crc]))
             tmp = self.sp.timeout
             self.sp.timeout = 30
-            print "Extended erase (0x44), this can take ten seconds or more"
+            print("Extended erase (0x44), this can take ten seconds or more")
             self._wait_for_ask("0x44 erasing failed")
             self.sp.timeout = tmp
             mdebug(10, "    Extended Erase memory done")
@@ -248,12 +335,12 @@ class CommandInterface:
     def cmdWriteProtect(self, sectors):
         if self.cmdGeneric(0x63):
             mdebug(10, "*** Write protect command")
-            self.sp.write(chr((len(sectors)-1) & 0xFF))
+            self.sp.write(bytes([(len(sectors)-1) & 0xFF]))
             crc = 0xFF
             for c in sectors:
                 crc = crc ^ c
-                self.sp.write(chr(c))
-            self.sp.write(chr(crc))
+                self.sp.write(bytes([c]))
+            self.sp.write(bytes([c]))
             self._wait_for_ask("0x63 write protect failed")
             mdebug(10, "    Write protect done")
         else:
@@ -288,9 +375,14 @@ class CommandInterface:
 
 
 # Complex commands section
+    def read_id(self):
+        self.sp.write(bytes([0x9F]))
+        self.sp.write(bytes([0x60]))
+        rx = self.sp.read(3)
+        print(rx.hex())
 
     def readMemory(self, addr, lng):
-        data = []
+        data = ''
         if usepbar:
             widgets = ['Reading: ', Percentage(),', ', ETA(), ' ', Bar()]
             pbar = ProgressBar(widgets=widgets,maxval=lng, term_width=79).start()
@@ -327,22 +419,21 @@ class CommandInterface:
             offs = offs + 256
             addr = addr + 256
             lng = lng - 256
+        if lng>0:
+            self.cmdWriteMemory(addr, data[offs:offs+lng] + bytes([0xFF] * (256-lng)) )
         if usepbar:
             pbar.update(pbar.maxval-lng)
             pbar.finish()
         else:
             mdebug(5, "Write %(len)d bytes at 0x%(addr)X" % {'addr': addr, 'len': 256})
-        self.cmdWriteMemory(addr, data[offs:offs+lng] + ([0xFF] * (256-lng)) )
+        
 
-
-
-
-	def __init__(self) :
+def __init__(self) :
         pass
 
 
 def usage():
-    print """Usage: %s [-hqVewvr] [-l length] [-p port] [-b baud] [-a addr] [-g addr] [file.bin]
+    print("""Usage: %s [-hqVewvr] [-l length] [-p port] [-b baud] [-a addr] [-g addr] [file.bin]
     -h          This help
     -q          Quiet
     -V          Verbose
@@ -353,12 +444,13 @@ def usage():
     -l length   Length of read
     -p port     Serial port (default: /dev/tty.usbserial-ftCYPMYJ)
     -b baud     Baud speed (default: 115200)
+    -t parity   N(none),E(Even),O(Odd)
     -a addr     Target address
     -g addr     Address to start running at (0x08000000, usually)
 
     ./stm32loader.py -e -w -v example/main.bin
 
-    """ % sys.argv[0]
+    """ % sys.argv[0])
 
 
 if __name__ == "__main__":
@@ -367,28 +459,31 @@ if __name__ == "__main__":
     try:
         import psyco
         psyco.full()
-        print "Using Psyco..."
+        print("Using Psyco...")
     except ImportError:
         pass
 
     conf = {
-            'port': '/dev/tty.usbserial-ftCYPMYJ',
+            'port': 'COM7',
             'baud': 115200,
-            'address': 0x08000000,
+            'parity': 'N',
+            'address': 0x90000000,
+            'read_id': 0,
             'erase': 0,
             'write': 0,
             'verify': 0,
             'read': 0,
             'go_addr':-1,
+            'len':16,
         }
 
 # http://www.python.org/doc/2.5.2/lib/module-getopt.html
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hqVewvrp:b:a:l:g:")
-    except getopt.GetoptError, err:
+        opts, args = getopt.getopt(sys.argv[1:], "hqVewvrip:t:b:a:l:g:")
+    except(getopt.GetoptError, err):
         # print help information and exit:
-        print str(err) # will print something like "option -a not recognized"
+        print( str(err) ) # will print something like "option -a not recognized"
         usage()
         sys.exit(2)
 
@@ -410,10 +505,14 @@ if __name__ == "__main__":
             conf['verify'] = 1
         elif o == '-r':
             conf['read'] = 1
+        elif o == '-i':
+            conf['read_id'] = 1
         elif o == '-p':
             conf['port'] = a
         elif o == '-b':
             conf['baud'] = eval(a)
+        elif o == '-t':
+            conf['parity'] = a
         elif o == '-a':
             conf['address'] = eval(a)
         elif o == '-g':
@@ -424,13 +523,13 @@ if __name__ == "__main__":
             assert False, "unhandled option"
 
     cmd = CommandInterface()
-    cmd.open(conf['port'], conf['baud'])
+    cmd.open(conf['port'], conf['baud'], conf['parity'])
     mdebug(10, "Open port %(port)s, baud %(baud)d" % {'port':conf['port'], 'baud':conf['baud']})
     try:
-        try:
-            cmd.initChip()
-        except:
-            print "Can't init. Ensure that BOOT0 is enabled and reset device"
+        #try:
+        cmd.initChip()
+        #except:
+        #    print("Can't init. Ensure that BOOT0 is enabled and reset device")
 
 
         bootversion = cmd.cmdGet()
@@ -442,34 +541,53 @@ if __name__ == "__main__":
 #    cmd.cmdReadoutUnprotect()
 #    cmd.cmdWriteUnprotect()
 #    cmd.cmdWriteProtect([0, 1])
-
-        if (conf['write'] or conf['verify']):
-            data = map(lambda c: ord(c), file(args[0], 'rb').read())
+        data = None
+        
+        if (conf['write'] or conf['verify'] or conf['erase']):
+            with open(args[0],'rb') as f:
+                data = f.read()
+            #data = map(lambda c: ord(c), file(args[0], 'rb').read())
 
         if conf['erase']:
-            cmd.cmdEraseMemory()
+            erase_sectors = prepare_sectors(conf['address'],data)
+            #print(erase_sectors)
+            cmd.cmdEraseMemory(erase_sectors)
 
         if conf['write']:
             cmd.writeMemory(conf['address'], data)
 
         if conf['verify']:
             verify = cmd.readMemory(conf['address'], len(data))
-            if(data == verify):
-                print "Verification OK"
+            if(data.hex() == verify):
+                print("Verification OK")
             else:
-                print "Verification FAILED"
-                print str(len(data)) + ' vs ' + str(len(verify))
-                for i in xrange(0, len(data)):
-                    if data[i] != verify[i]:
-                        print hex(i) + ': ' + hex(data[i]) + ' vs ' + hex(verify[i])
+                print("Verification FAILED")
+                print(str(len(data)) + ' vs ' + str(len(verify)/2) )
+                same = 0
+                unsame = 0
+                for i in range(0, len(data)):
+                    try:
+                        if data[i] != int(verify[i*2:i*2+2],16):
+                            unsame += 1
+                            if(unsame == 256):
+                                print("...")
+                            elif(unsame < 256):
+                                print( hex(i) + ': ' + hex(data[i]) + ' vs 0x' + verify[i*2:i*2+2] )
+                        else:
+                            same +=1
+                    except:
+                        print(f'verify:i={i}-'+verify[i*2:i*2+2])
+                print(f"same={same}, difference={unsame}")
 
         if not conf['write'] and conf['read']:
             rdata = cmd.readMemory(conf['address'], conf['len'])
-            file(args[0], 'wb').write(''.join(map(chr,rdata)))
+            #file(args[0], 'wb').write(''.join(map(chr,rdata)))
 
         if conf['go_addr'] != -1:
             cmd.cmdGo(conf['go_addr'])
+        
+        if conf['read_id']:
+            cmd.read_id()
 
     finally:
         cmd.releaseChip()
-
